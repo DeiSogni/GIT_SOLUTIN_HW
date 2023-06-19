c53787c36914eddfe98c19fa353be94f06c2b553
Alexander <WebDer11@gmail.com>
2023-06-19 17:09:30 +0400

    1

--- src/test/java/gitHubTest/DragAndDrop.java
+++ src/test/java/gitHubTest/DragAndDrop.java
@@ -5,8 +5,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @BeforeAll
@@ -21,13 +21,18 @@ public class DragAndDrop {
     void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
          $("#column-a").dragAndDropTo($("#column-b"));
-        $("#column-a").shouldHave(Condition.text("B"));
-        $("#column-b").shouldHave(Condition.text("A"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 
     @Test
     void action() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
 
 
     }
